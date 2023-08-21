023a8d3b25af459fa3fb21f6b2c2dd565337b4d6
awais1379 <awais.ali1379@gmail.com>
2023-08-21 17:15:51 +0500

    Ran create-react-app to create the react app

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      {/* Your component JSX content goes here */}
+      <h1>Hello, React!</h1>
+    </div>
+  );
+};
+
+export default Navbar;
