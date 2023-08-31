e551001f472e069008b42b52b2c0e3fa12ca4a92
awais1379 <awais.ali1379@gmail.com>
2023-09-01 04:31:36 +0500

    Worked on package details page

---
+++ frontend/src/components/aboutpackage.js
@@ -0,0 +1,143 @@
+import React from "react";
+
+const AboutPackage = () => {
+  return (
+    <>
+      <div className="tour_head1">
+        <table>
+          <tbody>
+            <tr>
+              <th>Places covered</th>
+              <th className="event-res">Description</th>
+              <th className="event-res">Activities</th>
+              <th>Island</th>
+            </tr>
+            <tr>
+              <td>Itabaca Channel</td>
+              <td className="event-res">Narrow water crossing</td>
+              <td className="event-res">Cross the channel by boat</td>
+              <td>Baltra</td>
+            </tr>
+            <tr>
+              <td>Rancho Primicias</td>
+              <td className="event-res">Giant tortoise ranch</td>
+              <td className="event-res">Hike along giant tortoises</td>
+              <td>Santa Cruz</td>
+            </tr>
+            <tr>
+              <td>Upper Part</td>
+              <td className="event-res">Island vantage point</td>
+              <td className="event-res">Complete island lookout</td>
+              <td>Santa Cruz</td>
+            </tr>
+            <tr>
+              <td>Los Gemelos</td>
+              <td className="event-res">Dual volcano craters</td>
+              <td className="event-res">Crater lookout</td>
+              <td>Santa Cruz</td>
+            </tr>
+            <tr>
+              <td>Puerto Ayora</td>
+              <td className="event-res">Small Town and Port</td>
+              <td className="event-res">Food, Arts and Crafts</td>
+              <td>Santa Cruz</td>
+            </tr>
+            <tr>
+              <td>Tortuga Bay</td>
+              <td className="event-res">White sand beach</td>
+              <td className="event-res">Marine iguanas and surfing</td>
+              <td>Santa Cruz</td>
+            </tr>
+            <tr>
+              <td>Charles Darwin Research Station</td>
+              <td className="event-res">Wildlife research station</td>
+              <td className="event-res">Darwin's footsteps</td>
+              <td>Santa Cruz</td>
+            </tr>
+            <tr>
+              <td>Puerto Villamil</td>
+              <td className="event-res">Small Town and Port</td>
+              <td className="event-res">Food, Arts and Crafts</td>
+              <td>Isabela</td>
+            </tr>
+            <tr>
+              <td>Los Túneles</td>
+              <td className="event-res">Ocean Rock Formations</td>
+              <td className="event-res">Snorkeling/Wildlife</td>
+              <td>Isabela</td>
+            </tr>
+            <tr>
+              <td>Laguna de Flamingos</td>
+              <td className="event-res">Mangrove lagoon</td>
+              <td className="event-res">Wildlife sightings</td>
+              <td>Isabela</td>
+            </tr>
+            <tr>
+              <td>Playa del Amor</td>
+              <td className="event-res">White sand beach</td>
+              <td className="event-res">Leisure</td>
+              <td>Isabela</td>
+            </tr>
+            <tr>
+              <td>Sierra Negra Volcano</td>
+              <td className="event-res">Volcano crater</td>
+              <td className="event-res">Hike/Bike/Sightseeing</td>
+              <td>Isabela</td>
+            </tr>
+            <tr>
+              <td>Wall of Tears</td>
+              <td className="event-res">Man-made structures</td>
+              <td className="event-res">Hike/Bike/Sightseeing</td>
+              <td>Isabela</td>
+            </tr>
+            <tr>
+              <td>Isabela Tortoise Center</td>
+              <td className="event-res">Wildlife research and breeding</td>
+              <td className="event-res">Learning and awareness</td>
+              <td>Isabela</td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        <p>
+          <strong>Inclusions</strong>
+        </p>
+        <p>
+          - Roundtrip plane tickets Quito-Baltra-Isabela
+          <br />
+          - Roundtrip transfer to Baltra airport at the time of arrival and
+          departure
+          <br />
+          - All meals detailed in the itinerary
+          <br />
+          - Softdrinks mentioned in the itinerary
+          <br />
+          - 4-Star Accomodation Facilities: single, double or triple
+          <br />
+          - Certified Galapagos National Park guidance - English/Spanish
+          <br />
+          - All activities described in the itinerary
+          <br />- All equipment needed for water activities: masks, snorkels,
+          fins, life jackets, etc.
+        </p>
+        <p>
+          <strong>Exclusions</strong>
+        </p>
+        <p>
+          - Alcoholic beverages
+          <br />
+          - Specific foods and beverages not mentioned in the itinerary.
+          <br />
+          - Entrance fee to the Galapagos National Park: $ 100 per person (cash
+          only)
+          <br />
+          - Immigration control card / INGALA transit card: $ 20 per person
+          (cash only)
+          <br />- Personal travel and health insurance
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AboutPackage;
--- frontend/src/pages/packagedetails.js
+++ frontend/src/pages/packagedetails.js
@@ -6,8 +6,18 @@ import Thumbnail from "../components/thumbnail";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import umbrella from "../assets/images/umbrella.png";
 import bookbg from "../assets/images/book-bg.png";
+import tick from "../assets/images/tick.png";
+import { BsFacebook } from "react-icons/bs";
+import { FaGooglePlus } from "react-icons/fa";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { BsLinkedin } from "react-icons/bs";
+import { FaWhatsappSquare } from "react-icons/fa";
+import { BsWhatsapp } from "react-icons/bs";
+import CarouselComponent from "../components/carouselcomponent";
+import AboutPackage from "../components/aboutpackage";
 
 const PackagesDetails = () => {
   return (
@@ -69,12 +79,106 @@ const PackagesDetails = () => {
                 <img src={umbrella}></img>
                 <h4>Photo Gallery</h4>
               </div>
+              <div className="carousel-container">
+                <CarouselComponent></CarouselComponent>
+              </div>
+              <div className="umbrella-header">
+                <img src={umbrella}></img>
+                <h4>Location</h4>
+              </div>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d936560.5681945613!2d-90.70041025498932!3d-0.6568819820470502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9aaa5f3c9454fba5%3A0x900d7fee5795677f!2sSanta%20Cruz%20Island!5e0!3m2!1sen!2sus!4v1630763724768!5m2!1sen!2sus"
+                width="100%"
+                height="350"
+                style={{ border: "0" }}
+                allowfullscreen=""
+                loading="lazy"
+              ></iframe>
+              <div className="umbrella-header">
+                <img src={umbrella}></img>
+                <h4>About The Package</h4>
+              </div>
+              <AboutPackage/>
+              <div className="umbrella-header">
+                <img src={umbrella}></img>
+                <h4>Detailed Day Wise Itinerary</h4>
+              </div>
             </div>
             <div className="pd-right">
-                <img src={bookbg}></img>
-                <div className="pd-highlights">
-                    <h3>PACKAGE HIGHLIGHTS</h3>
+              <img src={bookbg} className="pd-book-img"></img>
+              <div className="pd-highlights">
+                <h3>PACKAGE HIGHLIGHTS</h3>
+                <div className="pd-facilities">
+                  <div className="pd-tick">
+                    <img src={tick} className="pd-tick-img"></img>
+                    <h5>Location : Rio,Brazil</h5>
+                  </div>
+                  <div className="pd-tick">
+                    <img src={tick} className="pd-tick-img"></img>
+                    <h5>Arrival Date: Nov 12, 2017</h5>
+                  </div>
+                  <div className="pd-tick">
+                    <img src={tick} className="pd-tick-img"></img>
+                    <h5>Departure Date: Nov 21, 2017</h5>
+                  </div>
+                  <div className="pd-tick">
+                    <img src={tick} className="pd-tick-img"></img>
+                    <h5>Free Sightseeing & Hotel</h5>
+                  </div>
                 </div>
+              </div>
+              <div className="pd-highlights">
+                <h3>Share This Package</h3>
+                <div className="pd-social">
+                  <BsFacebook size={"3rem"} color="#3b5998" />
+                  <FaGooglePlus size={"3rem"} color="#DB4437" />
+                  <AiFillTwitterCircle size={"3rem"} color="#00acee" />
+                  <BsLinkedin size={"3rem"} color=" #0072b1" />
+                  <FaWhatsappSquare size={"3rem"} color="#128C7E" />
+                </div>
+              </div>
+              <div className="pd-highlights">
+                <h3>Help & Support 24/7</h3>
+                <div className="pd-help">
+                  <div className="wa-title">
+                    <BsWhatsapp size={"24px"} color="#dd4b39" />
+                    <h4>WhatsApp Chat</h4>
+                  </div>
+                  <h4 className="help-num">+593-99-354-6632</h4>
+                </div>
+              </div>
+              <div className="pd-highlights">
+                <h3>Popular Packages</h3>
+                <div className="popular-container">
+                  <div className="popular-packs">
+                    <img src={bookbg} className="popular-img"></img>
+                    <h4>Dubai 7Days / 6Nights</h4>
+                    <p>
+                      Many desktop publishing packages and web page editors now
+                      use Lorem Ipsum as their default model text
+                    </p>
+                    <a>View This Package</a>
+                  </div>
+                  <div className="popular-packs">
+                    <img src={bookbg} className="popular-img"></img>
+                    <h4>Dubai 7Days / 6Nights</h4>
+                    <p>
+                      Many desktop publishing packages and web page editors now
+                      use Lorem Ipsum as their default model text
+                    </p>
+                    <a>View This Package</a>
+                  </div>
+                  <div className="popular-packs">
+                    <img src={bookbg} className="popular-img"></img>
+                    <h4>Dubai 7Days / 6Nights</h4>
+                    <p>
+                      Many desktop publishing packages and web page editors now
+                      use Lorem Ipsum as their default model text
+                    </p>
+                    <a>View This Package</a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
