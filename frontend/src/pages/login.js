3f413355c335452fad5ac5f1643bcb87d31c46e1
areejehsan95 <areejehsan95@gmail.com>
2023-11-21 13:01:35 +0500

    register.js

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -4,16 +4,56 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import React, { useEffect, useState } from "react";
 
-// import packageService from "../services/package-service";
+import packageService from "../services/package-service";
 
 const Login = () => {
 
-  const [packages, setPackages] = useState([])
+  const [user, setUser] = useState({
+    username: null,
+    password: null
+  });
 
-  useEffect(()=>{
-    
-  },[])
+  useEffect(() => {
 
+  }, []);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    loginUser();
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value
+    }));
+  };
+
+  const loginUser = async () => {
+    console.log(user)
+    //user service
+    // try {
+    //   const response = await fetch('YOUR_API_ENDPOINT', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(user),
+    //   });
+
+    //   if (response.ok) {
+    //     const data = await response.json();
+    //     console.log('Login successful:', data);
+    //   } else {
+    //     console.error('Login failed:', response.statusText);
+    //   }
+    // } catch (error) {
+    //   console.error('An error occurred during login:', error.message);
+    // }
+  };
+
+  
   return (
     <>
       <Navbar />
@@ -21,15 +61,15 @@ const Login = () => {
         <div className="tr-regi-form">
           <h4>Sign In</h4>
           <p>Explore Ecuador's most innovative<br />destination experience offer</p>
-          <form className="col s12">
+          <form className="col s12" onSubmit={handleFormSubmit}>
             <div className="row">
               <div className="input-field col s12">
-                <input type="text" className="validate" placeholder="User Name" required />
+                <input type="text" className="validate" value={user.username} name="username" onChange={handleInputChange} placeholder="User Name" required />
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input type="password" className="validate" placeholder="Password" required />
+                <input type="password" className="validate" value={user.password} name="password" onChange={handleInputChange} placeholder="Password" required />
               </div>
             </div>
             <div className="row">
@@ -44,9 +84,9 @@ const Login = () => {
           <div className="soc-login">
             <h4>Sign in using</h4>
             <ul>
-              <li><a href="#"><i className="fab fa-facebook fb1"></i> Facebook</a> </li>
-              <li><a href="#"><i className="fab fa-twitter tw1"></i> Twitter</a> </li>
-              <li><a href="#"><i className="fab fa-google-plus gp1"></i> Google</a> </li>
+              <li><a href="https://www.facebook.com/"><i className="fab fa-facebook fb1"></i> Facebook</a> </li>
+              <li><a href="https://twitter.com/"><i className="fab fa-twitter tw1"></i> Twitter</a> </li>
+              <li><a href="https://google.com/"><i className="fab fa-google-plus gp1"></i> Google</a> </li>
             </ul>
           </div>
         </div>
--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -8,7 +8,54 @@ import React, { useEffect, useState } from "react";
 
 const Register = () => {
 
-  const [packages, setPackages] = useState([])
+  const [user, setUser] = useState({
+    id: null,
+    phone: null,
+    email: null,
+    idType: null,
+    password: null,
+    firstname: null,
+    lastname:  null,
+    nationality: null,
+    confirmPassword: null,
+  });
+
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    registerUser();
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value
+    }));
+  };
+
+  const registerUser = async () => {
+    console.log(user)
+    //user service
+    // try {
+    //   const response = await fetch('YOUR_API_ENDPOINT', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(user),
+    //   });
+
+    //   if (response.ok) {
+    //     const data = await response.json();
+    //     console.log('Login successful:', data);
+    //   } else {
+    //     console.error('Login failed:', response.statusText);
+    //   }
+    // } catch (error) {
+    //   console.error('An error occurred during login:', error.message);
+    // }
+  };
 
   return (
     <>
@@ -17,23 +64,23 @@ const Register = () => {
         <div className="tr-regi-form">
           <h4>Create an Account</h4>
           <p>It's free and always will be.</p>
-          <form className="col s12">
+          <form className="col s12" onSubmit={handleFormSubmit}>
             <div className="row">
               <div className="input-field col m6 s12">
-                <input type="text" className="validate" placeholder="First Name" required />
+                <input type="text" className="validate" value={user.firstname} name="firstname" onChange={handleInputChange} placeholder="First Name" required />
               </div>
               <div className="input-field col m6 s12">
-                <input type="text" className="validate" placeholder="Last Name" required />
+                <input type="text" className="validate" value={user.lastname} name="lastname" onChange={handleInputChange} placeholder="Last Name" required />
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input type="number" className="validate" placeholder="Nationality" required />
+                <input type="number" className="validate" value={user.nationality} name="nationality" onChange={handleInputChange} placeholder="Nationality" required />
               </div>
             </div>
             <div className="row">
-              <div className="input-field col s6">
-                <select required>
+              <div className="input-field col s6 bf-row-single">
+                <select required value={user.idType} name="idType" onChange={handleInputChange}>
                   <option value="" disabled selected>
                     Type of ID
                   </option>
@@ -44,43 +91,43 @@ const Register = () => {
                   </option>
                 </select>
 
-
-                <div class="custom-select-wrapper">
+               
+                {/* <div class="custom-select-wrapper">
                   <input type="text" class="custom-select-input" value="Type of ID" />
-                  <ul class="custom-dropdown-content" style={{"width": "406.25px", "position": "absolute", "top": "0px", "left": "0px", "opacity": 1}}>
+                  <ul class="custom-dropdown-content" style={{"width": "406.25px", "top": "0px", "left": "0px", "opacity": 1}}>
                     <li class="disabled"><span>Type of ID</span></li>
                     <li class=""><span>Passport</span></li>
                     <li class="active selected"><span>Driver's License</span></li>
                     <li class=""><span>Other Government Issued ID</span></li>
                   </ul>
-                </div>
+                </div> */}
 
               </div>
               <div className="input-field col s6">
-                <input type="number" className="validate" name="id" placeholder="Enter your ID number" required />
+                <input type="number" className="validate" value={user.id} name="id" onChange={handleInputChange} placeholder="Enter your ID number" required />
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input type="number" className="validate" placeholder="Enter your phone" />
+                <input type="number" className="validate" value={user.phone} name="phone" onChange={handleInputChange} placeholder="Enter your phone" />
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input type="email" className="validate" placeholder="Enter your email" required />
+                <input type="email" className="validate" value={user.email} name="email" onChange={handleInputChange} placeholder="Enter your email" required />
               </div>
             </div>
             <div className="row">
               <div className="input-field col m6 s12">
-                <input type="password" className="validate" placeholder="Password" required />
+                <input type="password" className="validate" value={user.password} name="password" onChange={handleInputChange} placeholder="Password" required />
               </div>
               <div className="input-field col m6 s12">
-                <input type="password" className="validate" placeholder="Confirm Password" required />
+                <input type="password" className="validate" value={user.confirmPassword} name="confirmPassword" onChange={handleInputChange} placeholder="Confirm Password" required />
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input type="submit" value="Register" className="waves-effect waves-light btn-large full-btn" />
+                <input type="submit" value="Register" className="waves-effect waves-light btn-large full-btn" onSubmit={registerUser} />
               </div>
             </div>
           </form>          
